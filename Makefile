60f30e0d711a5a09e66c25a5533d8f89600f48a4
Joeldoslov <84170133+bruhmese-python@users.noreply.github.com>
2025-06-30 22:30:32 +0530

    sitemap makefile

--- Makefile
+++ Makefile
@@ -1,11 +1,26 @@
-all: sitemap.html
-	jinja2 _index.html > index.html
-	jinja2 _downloads.html > downloads.html
-	jinja2 _links.html > links.html
-	jinja2 _sitemap.html > sitemap.html
+# Variables
+JINJA_CMD = jinja2
+PYTHON_CMD = python3.9
 
-sitemap.html: 
-	python3.9 sitemap_generator.py > sitemap.html.tmp
+# Find all template files
+TEMPLATES = $(wildcard _*.html)
 
+# Generate target files by replacing _ with nothing in template filenames
+TARGETS = $(patsubst _%.html,%.html,$(TEMPLATES))
+
+# Default target
+all: $(TARGETS) sitemap.html
+
+# Rule to generate target files from templates
+%.html: _%.html
+	$(JINJA_CMD) $< > $@
+
+# Special rule for sitemap.html
+sitemap.html: _sitemap.html sitemap_generator.py
+	$(JINJA_CMD) _sitemap.html > sitemap.html
+	sleep 1	
+	$(PYTHON_CMD) sitemap_generator.py > sitemap.html.tmp
+
+# Clean target
 clean:
-	rm index.html downloads.html links.html sitemap.html
+	rm -f $(TARGETS)
--- sitemap_generator.py
+++ sitemap_generator.py
@@ -1,5 +1,44 @@
 import subprocess
 import json
+import os
+import re
+
+def filter_items(items, exclude_patterns):
+    filtered_items = []
+    
+    for item in items:
+        if any(re.match(pattern, item) for pattern in exclude_patterns):
+            # print(f"Ignoring item: {item}")
+            continue
+        filtered_items.append(item)
+    
+    return filtered_items
+
+def read_file_to_list(filename):
+    if not os.path.exists(filename):
+        # print(f"Error: The file '{filename}' does not exist.")
+        return []
+
+    try:
+        with open(filename, 'r') as file:
+            lines = file.readlines()
+            
+            if not lines:
+                # print(f"Warning: The file '{filename}' is empty.")
+                return []
+            
+            lines = [line.strip() for line in lines]
+
+            return lines
+
+    except IOError as e:
+        # print(f"Error: An I/O error occurred while reading the file '{filename}'. Details: {e}")
+        return []
+
+filename = 'sitemap.exclude'
+exclude_patterns = read_file_to_list(filename)
+
+# print('exclude_patterns:',exclude_patterns)
 
 TREE_SYMBOLS = {
     'branch': '├──',
@@ -14,35 +53,44 @@ def generate_sitemap_from_json(tree_json, depth=0, is_last=False):
     global parents
     html_content = []
 
+    name_list = [x['name'] for x in tree_json]
+    # print('name list:',name_list)
+    # print('tree json:',tree_json)
+
+    filtered_items = filter_items(name_list, exclude_patterns)
+    # print('filtered items:',filtered_items)
+
     for index, item in enumerate(tree_json):
         name = item['name']
-        item_type = item['type']
+        if name in filtered_items:
+            item_type = item['type']
 
-        is_last_item = (index == len(tree_json) - 1)
+            is_last_item = filtered_items[-1] == name
 
-        indent = '  ' * depth
-        tree_symbol = TREE_SYMBOLS['last_branch'] if is_last_item else TREE_SYMBOLS['branch']
-        
-        if item_type == 'directory':
-            parents.append(name)
-            details = f'{indent}{tree_symbol}<details><summary class="sitemap-folder"><img width="16" src="images/dir.png" class>&nbsp;{name}</summary>'
+            indent = '  ' * depth
+            tree_symbol = TREE_SYMBOLS['last_branch'] if is_last_item else TREE_SYMBOLS['branch']
+            
+            if item_type == 'directory':
+                parents.append(name)
+                details = f'{indent}{tree_symbol}<details><summary class="sitemap-folder"><img width="16" src="images/dir.png" class>&nbsp;{name}</summary>'
 
-            if 'contents' in item:
-                content = generate_sitemap_from_json(item['contents'], depth + 1, is_last_item)
-                if content:
-                    details+=content
-                else:
-                    parents = parents[:-1]
-            details+='</details>'
-            html_content.append(details)
+                if 'contents' in item:
+                    content = generate_sitemap_from_json(item['contents'], depth + 1, is_last_item)
+                    if content:
+                        details+=content
+                    else:
+                        parents = parents[:-1]
+                details+='</details>'
+                html_content.append(details)
 
-        elif item_type == 'file':
-            href = ('/'.join(parents) + '/' + name) if parents else name
-            html_content.append(f'{indent}{tree_symbol} <a href="{href}" class="sitemap-anchor">{name}</a>')
+            elif item_type == 'file':
+                href = ('/'.join(parents) + '/' + name) if parents else name
+                html_content.append(f'{indent}{tree_symbol} <a href="{href}" class="sitemap-anchor">{name}</a>')
 
-            if parents: 
-                if is_last or index == len(tree_json) - 1:
-                    parents = parents[:-1]
+                if parents: 
+                    if is_last or index == len(tree_json) - 1:
+                        parents = parents[:-1]
+
 
     return '\n'.join(html_content)
 
