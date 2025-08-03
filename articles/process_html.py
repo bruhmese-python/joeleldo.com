555d07724f69ea2b02f56798cc8b329b69ca777a
Joeldoslov <84170133+bruhmese-python@users.noreply.github.com>
2025-08-03 21:56:47 +0530

    dashboard

--- articles/process_html.py
+++ articles/process_html.py
@@ -40,11 +40,19 @@ def process_snippets(file_path :str):
     with open(file_path, 'r', encoding='utf-8') as file:
         code_highlighted_html = file.read()
 
-    # find span tags with content of the format .*\..*
+    # find p tags with content of the format .*\..*
     pattern = r'<p[^>]*>\s*[^\.]*[\w\-\.]+\.[\w]+\s*</p>'
-    matches = re.findall(pattern, code_highlighted_html)
+    matches = list(re.findall(pattern, code_highlighted_html))
 
-    print('Snippets found:', list(matches))
+    # find span tags with content of the format .*\..*
+    pattern = r'<span[^>]*>\s*[^\.]*[\w\-\.]+\.[\w]+\s*</span>'
+    matches += list(re.findall(pattern, code_highlighted_html))
+
+    snippet_files = list(map(lambda x: x.split('>')[1].split('<')[0] , matches))
+ 
+    print('Snippets found:')
+    [print(f"\t{x}") for x in snippet_files]
+
     #if files with their content's fielnames exist in the current directory, replace them with the content inside the span tag
     for match in matches:
         file_name = match.split('>')[1].split('<')[0]
